78d7c46e975e010691cca6fefc0b131757436710
Genesis Jesus <genesisdev96@outlook.com>
2019-02-23 13:35:55 -0600

    create event to add a task

--- app/js/app.js
+++ app/js/app.js
@@ -11,3 +11,33 @@ class Utils{
         document.getElementById('description').value = '';
     }
 }
+
+document.getElementById('formAdd').addEventListener('submit', (e) =>{
+    
+    // PreventDefault
+    e.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+
+    // Validate fields
+    if(title === '' || description === ''){
+        // Message for empty fields
+        Utils.showMessage('The fields are empty', 'danger')
+    }else{
+        const task = new Task(title, description);
+
+        // Add task to the UI
+        UI.addTaskToList(task);
+
+        // Add task to the localstorage
+        Store.addTaskToLocalStorage(task);
+
+        // Show success message
+        Utils.showMessage('The task was added correctly', 'success');
+
+        // Crear form fields
+        Utils.clearFields();
+
+    }
+});
